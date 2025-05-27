521c688f02a95e2ff9848aaf07a50153b8c0cbf2
Heymun Pareek <78228395+PassionateCoder340@users.noreply.github.com>
2025-05-27 13:22:00 +0530

    Added custom http server functionals

--- callback.functions.js
+++ callback.functions.js
@@ -4,6 +4,9 @@
 * @description war: write, append, read
 */
 
+const fs = require('fs');
+const path = require('path');
+
 function war(err, data) {
     if (err) {
         console.error(`Error: ${err}`);
@@ -17,6 +20,56 @@ function war(err, data) {
     }
     
 }
+
+function listener(PORT){
+    return () => {
+    console.log(`Server is listening on port ${PORT}`);
+    }
+}
+
+function defaulthttpServer(html_path_arr, path_routes) {
+    return (req, res) => {
+    if (!html_path_arr || !path_routes || html_path_arr.length !== path_routes.length) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error: Mismatched paths and routes');
+        return;
+    }
+    const basePath = path.join(__dirname, html_path_arr[0]);
+    if (req.url == path_routes[0]) {
+        fs.readFile(basePath, 'utf-8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+        return;
+    }
+    for (let i = 1; i < path_routes.length; i++) {
+        if (req.url == path_routes[i]) {
+            const filePath = path.join(__dirname, html_path_arr[i]);
+            fs.readFile(filePath, 'utf-8', (err, data) => {
+                if (err) {
+                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                    res.end('Internal Server Error');
+                    return;
+                }
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            });
+            return;
+        }
+    }
+    // Only send 404 if no route matched
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+    };
+}
+
 module.exports = {
-    war
+    war,
+    listener,
+    defaulthttpServer
 };
--- index.js
+++ index.js
@@ -12,13 +12,25 @@
  */
 
 const fs = require('fs');
+const http = require('http');
 const cfns = require('./callback.functions.js');
+const path = require('path');
 
 /**
 * * @description example 1
-    fs.writefile('./test.txt', "Hello World!", cfns.war);
+    fs.writeFile('./test.txt', "Hello World!", cfns.war);
     fs.readFile("./test.txt", "utf-8", cfns.war);
     fs.appendFile("./test.txt", "Hello World!", cfns.war);
     fs.readFile("./test.txt", "utf-8", cfns.war);
 */
+/**
+ * @description example 2
+ * @param {number} PORT - The port number on which the server will listen.
+ */
+
+html_path_arr = ['./html_files/index1.html', './html_files/index2.html'];
+routes_arr = ['/','/about'];
+
+const server = http.createServer(cfns.defaulthttpServer(html_path_arr, routes_arr));
+server.listen(3000, cfns.listener(3000));
 
